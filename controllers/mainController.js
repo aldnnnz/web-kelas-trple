b37a74e9e2110ad9103b8cac8f4d57cbac6ca72b
maell <aldynnn@yahoo.com>
2023-11-11 07:43:26 +0700

    Revert "add create new user"
    This reverts commit 59fc460edfd4e084464f022b04f3cfa7c1f4135b.

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -3,7 +3,7 @@ const mainModel = require('../models/mainModel')
 const getAllUsers = async (req, res)  => {
     try {
         const users = await mainModel.getAllUsers(); // Memanggil model untuk mendapatkan data
-        res.render('main', { users }); // Merender tampiln EJS dengan data pengaguna
+        res.render('main', { users }); // Merender tampilan EJS dengan data pengguna
     } catch (error) {
         // Tangani kesalahan jika terjadi
         console.error(error);
@@ -11,28 +11,6 @@ const getAllUsers = async (req, res)  => {
     }
 };
 
-const createNewUser = async (req, res) => {
-    try {
-        // Ambil data pengguna 
-        const userData = {
-            nama: req.body.nama,
-            // Tambahkan data
-        };
-        const result = await mainModel.createNewUser(userData);
-        if (result) {
-            req.flash('success', 'Data pengguna berhasil disisipkan.');
-            res.redirect('/admin');
-        } else {
-            res.redirect('/admin');
-        }
-    } catch (error) {
-        console.error(error);
-        req.flash('error', 'Gagal menyisipkan data pengguna baru.');
-        res.status(500).send('Terjadi kesalahan saat menyisipkan data pengguna baru.');
-    }
-};
-
 module.exports = {
     getAllUsers,
-    createNewUser
 };
