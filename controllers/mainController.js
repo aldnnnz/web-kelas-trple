5d2a67d7b370ef54f944346141dc1ead80349f18
maell <aldynnn@yahoo.com>
2023-11-11 08:14:26 +0700

    feat: add new user

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -2,15 +2,35 @@ const mainModel = require('../models/mainModel')
 
 const getAllUsers = async (req, res)  => {
     try {
-        const users = await mainModel.getAllUsers(); // Memanggil model untuk mendapatkan data
-        res.render('main', { users }); // Merender tampilan EJS dengan data pengguna
+        const users = await mainModel.getAllUsers(); 
+        res.render('main', { users }); 
     } catch (error) {
-        // Tangani kesalahan jika terjadi
         console.error(error);
         res.status(500).send('Terjadi kesalahan saat mengambil data pengguna.');
     }
 };
 
+const createNewUser = async (req, res) => {
+    try {
+        const userData = {
+            nama: req.body.nama,
+            // Tambahkan data
+        };
+        const result = await mainModel.createNewUser(userData);
+        if (result) {
+            req.flash('success', 'Data pengguna berhasil disisipkan.');
+            res.redirect('/admin');
+        } else {
+            res.redirect('/admin');
+        }
+    } catch (error) {
+        console.error(error);
+        req.flash('error', 'Gagal menyisipkan data pengguna baru.');
+        res.status(500).send('Terjadi kesalahan saat menyisipkan data pengguna baru.');
+    }
+};
+
 module.exports = {
     getAllUsers,
+    createNewUser
 };
