6c17e61085ff98318f4eef3c10b44d4d4353d02c
aldnnnz <sayabukanrobot999@gmail.com>
2024-01-10 11:21:46 +0700

    fix

--- index.js
+++ index.js
@@ -14,21 +14,21 @@ const mahasiswa = require('./mahasiswa.controller');
 
 const port = process.env.PORT;
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(session({
-  secret: 'secret',
-  resave: false,
-  saveUninitialized: true,
-  cookie: {maxAge: 60000}
-}))
-app.use(flash());
-app.use(methodOverride("_method"));
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
+// app.use(session({
+//   secret: 'secret',
+//   resave: false,
+//   saveUninitialized: true,
+//   cookie: {maxAge: 60000}
+// }))
+// app.use(flash());
+// app.use(methodOverride("_method"));
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
 
 
 
